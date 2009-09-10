a38c380e74c7a4de6992c2bd7a7b927abbda1faf
markdpercival <markdpercival@691b3cc6-9d5a-11de-a3fa-a586d8720ea5>
2009-09-10 21:32:21 +0000

    initial commit
    git-svn-id: https://json-zips.googlecode.com/svn/trunk@2 691b3cc6-9d5a-11de-a3fa-a586d8720ea5

---
+++ src/zip_import.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'fastercsv'
+require 'json'
+
+file = "zips_better.csv"
+output_dir = '../zips/'
+
+csv = FasterCSV.read(file)
+csv.slice!(0)
+
+groups = {}
+
+csv.each do |z|
+  groups[z[0][0..2]] ||= {}
+  groups[z[0][0..2]][z[0]] = {:state => z[1], :city => z[4]}
+end
+groups.each_pair do |k,v|
+  f = File.new(output_dir + k + ".json",  "w")
+  f << "squarepush.jsonzips(#{v.to_json});"
+end
